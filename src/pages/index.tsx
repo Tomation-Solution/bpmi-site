35bbdf426694a66a77e13a283d92fbc29b5725fe
markothedev <markothedevmail@gmail.com>
2023-04-28 16:13:51 +0100

    added a new herosection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
       <main className={""}>
         <Layout>
           <Herofn
-            img={"allinsuit"}
+            img={"g"}
             title={
               "Promoting the practice of Business Process Management in Nigeria"
             }
